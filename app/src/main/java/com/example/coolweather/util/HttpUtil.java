58900f58df9695be46a7cb86fc4a7e09a2704ae5
C515 <C515>
2017-05-18 14:56:54 +0800

    First commit.

---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,14 @@
+package com.example.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by C515 on 2017/5/18.
+ */
+public class HttpUtil {
+    public static void  sendOkHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request()
+    }
+}
